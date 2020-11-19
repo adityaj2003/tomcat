915019260f5d3af8eab1c88f5a73e6b1b0f6b8ce
SalenaAshton <salena@email.arizona.edu>
2020-11-19 11:04:20 -0700

    cleaned up code and comments.

--- src/python/planner_modeling_mqtt.py
+++ src/python/planner_modeling_mqtt.py
@@ -1,28 +1,30 @@
 import paho.mqtt.client as mqtt
+import time
 
-# The callback for when the MQTTC receives a CONNACK response from the server.
-def on_connect(MQTTC, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-        # reconnect then subscriptions will be renewed.
-    MQTTC.subscribe("agents/tomcat_tmm")
+#  # Subscribing within on_connect() renewconnection if disconnected. 
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Now connected with result code "+str(rc))
+    else:
+        print("Bad connection. Result code =", rc)
+    client.subscribe("agents/tomcat_tmm")
 
 # The callback for when a PUBLISH message is received from the server.
-def on_message(MQTTC, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    # from elkless_replayer, line 88
-    MQTTC.publish("agents/tomcat_planner", "dummy output from planner")
+def on_message(client, userdata, msg):
+    print("on_message:", msg.topic+" "+str(msg.payload))
+    # arg 1 = topic.   arg 2 = "the message itself." 
+    client.publish("agents/tomcat_planner", "dummy output from planner from"
+                   " within planner_modeling_mqtt.py")
 
-
+# Create client
 MQTTC = mqtt.Client()
+
+# Binds callback functions. Without these, then the on_connect() and
+  #the on_message() functions won't be called.
 MQTTC.on_connect = on_connect
 MQTTC.on_message = on_message
 
+# Connects to broker
 MQTTC.connect("localhost", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-    # handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-    # manual interface.
 MQTTC.loop_forever()
