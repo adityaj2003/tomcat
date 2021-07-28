914507dda041db52507208cb5a98a5b2e01e796b
Vincent Raymond <vraymond6@arizona.edu>
2021-07-28 12:09:55 -0700

    Updated run_batch.sh

--- tools/replayer/tools/run_batch.sh
+++ tools/replayer/tools/run_batch.sh
@@ -1,1 +1,4 @@
-for FILE in trials/*; do ./replayer $FILE > $FILE.metadata_replay; done
+MQTT_HOST="localhost"
+MQTT_PORT=1883
+
+for FILE in trials/*; do ./replayer --trial_directory $FILE --mqtt_host $MQTT_HOST --mqtt_port $MQTT_PORT > $FILE.metadata_replay; done
