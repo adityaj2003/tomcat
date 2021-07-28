c4e749a00c33d2068e74501b10573f97cc03ea6d
Vincent Raymond <vraymond6@arizona.edu>
2021-07-28 11:56:38 -0700

    Added command mqtt_host mqtt_port command line options

--- tools/replayer/src/WebsocketClient.cpp
+++ tools/replayer/src/WebsocketClient.cpp
@@ -8,6 +8,8 @@
 #include <thread>
 #include <chrono>
 
+#include <boost/program_options.hpp>
+
 #include "AudioChunkPreprocessor.h"
 #include "MetadataPreprocessor.h"
 #include "Mosquitto.h"
@@ -18,38 +20,69 @@ namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
+using namespace boost::program_options;
 using namespace std;
-void run_session(vector<vector<int16_t>>, string);
+void run_session(vector<vector<int16_t>>, string, int);
 
 int main(int argc, char** argv)
 {
+    // Command line arguments
+    string trial_directory;
+    string mqtt_host;
+    int mqtt_port;
+
+     try {
+        options_description desc{"Options"};
+        desc.add_options()("help,h", "Help screen")(
+            "trial_directory",
+	    value<string>(&trial_directory)->default_value("NA"),
+	    "The directory for the trial to be replayed")(
+	    "mqtt_host",
+            value<string>(&mqtt_host)->default_value("localhost"),
+            "The host address of the mqtt broker")(
+            "mqtt_port",
+            value<int>(&mqtt_port)->default_value(1883),
+            "The port of the mqtt broker");
+
+        variables_map vm;
+        store(parse_command_line(argc, argv, desc), vm);
+        notify(vm);
+    }
+    catch (const error& ex) {
+        cout << "Error parsing arguments" << endl;
+        return -1;
+    }
+
     // Preprocess audio chunks
-    AudioChunkPreprocessor acp(argv[1]);
+    AudioChunkPreprocessor acp(trial_directory);
 
     // Preprocess metadata files
-    MetadataPreprocessor mdp(argv[1]);
+    MetadataPreprocessor mdp(trial_directory);
  
     // Create thread array
     vector<thread> thread_list;
     
     // Initialize mosquitto 
     MosquittoListener mqtt_client;
-    mqtt_client.connect("0.0.0.0", 1883, 1000, 1000, 1000);
+    mqtt_client.connect(mqtt_host, mqtt_port, 1000, 1000, 1000);
     mqtt_client.subscribe("#");
     mqtt_client.set_max_seconds_without_messages(2147483647); // Max Long value
     thread mqtt_thread = thread([&] { mqtt_client.loop(); });
     mqtt_client.start_local = chrono::system_clock::now();
 
-    
+    // Kill existing playback processes 
+    system("killall python");
+
     // Start metadata playback
-/*    thread playback_thread = thread([&] {
+    thread playback_thread = thread([&] {
 	    string command = "python tools/Playback.py " + mdp.metadata_filename + "_modified"; 
 	    system(command.c_str());
+	    cout << "replay complete" << endl;
     });
-*/
+
     // Initilize threads 
     for(int i=0; i<acp.num_participants;i++){
-	thread_list.push_back(thread(run_session, acp.audio_chunks[i], acp.participant_ids[i]));
+	thread_list.push_back(thread(run_session, acp.audio_chunks[i], acp.participant_ids[i], acp.sample_rates[i]));
     } 
 
     // Join threads 
@@ -60,12 +93,12 @@ int main(int argc, char** argv)
     mqtt_client.close();
     mqtt_thread.join();
 
-  //  playback_thread.join();    
+    playback_thread.join();    
     return EXIT_SUCCESS;
 }
 
-void run_session(vector<vector<int16_t>> audio_chunks, string participant_id){
-	int sample_rate = 48000;
+void run_session(vector<vector<int16_t>> audio_chunks, string participant_id, int sample_rate){
+        std::cout << sample_rate << std::endl;
 	int chunk_size = 4096;
 	double chunks_per_second = (double)sample_rate/(double)chunk_size;
 	double seconds_interval = 1.0/(double)chunks_per_second;
@@ -93,7 +126,7 @@ void run_session(vector<vector<int16_t>> audio_chunks, string participant_id){
 		    }));
 
 		// Perform the websocket handshake
-		ws.handshake("0.0.0.0", "/?sampleRate=48000&id=" + participant_id);
+		ws.handshake("0.0.0.0", "/?sampleRate=" + to_string(sample_rate) + "&id=" + participant_id);
 		
 		// Send the message
 		auto start = chrono::high_resolution_clock::now();
@@ -113,7 +146,6 @@ void run_session(vector<vector<int16_t>> audio_chunks, string participant_id){
 		auto stop = chrono::high_resolution_clock::now();
 		auto duration = chrono::duration_cast<std::chrono::microseconds>(stop-start);
 		
-		std::cout << duration.count() << std::endl;
 		// Close the WebSocket connection
 		ws.close(websocket::close_code::normal);
 
